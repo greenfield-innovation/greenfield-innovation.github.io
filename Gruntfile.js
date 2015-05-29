5c489602e04d818177169acb1dc679f773a930df
Monte Hayward <monte@monte15bookpro2.attlocal.net>
2015-05-29 18:23:55 -0500

    responsive-nav.js upgrade

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,62 +1,62 @@
 'use strict';
 module.exports = function(grunt) {
-    // Build File for a GitHub Pages Organization Page
-    // builds in stages, using grunt and jekyll
+  // Build File for a GitHub Pages Organization Page
+  // builds in stages, using grunt and jekyll
 
-  var jekyllConditionalWrapOpen = /\{\% if[^}]+\%\}/;
+  var jekyllConditionalWrapOpen  = /\{\% if[^}]+\%\}/;
   var jekyllConditionalWrapClose = /\{\%[^}]+endif \%\}/;
-  var jekyllConditionalWrapPair = [jekyllConditionalWrapOpen, jekyllConditionalWrapClose];
+  var jekyllConditionalWrapPair  = [jekyllConditionalWrapOpen, jekyllConditionalWrapClose];
+  var inProduction               = true;
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    jshint: {
+  grunt.initConfig( {
+    pkg:      grunt.file.readJSON( 'package.json' ),
+    jshint:   {
       options: {
         jshintrc: '.jshintrc'
       },
-      all: [
+      all:     [
         'Gruntfile.js',
         'assets/js/*.js',
         'assets/js/plugins/*.js',
         '!assets/js/scripts.min.js'
       ]
     },
-    less: {
+    less:     {
       dist: {
         options: {
-          compile: true,  // turn less into CSS
+          compile:  true,  // turn less into CSS
           compress: false
         },
-        files: {
-            'assets/css/main.css' : ['.src/assets/less/main.less'],
-            'assets/css/page.css' : ['.src/assets/less/page.less'],
+        files:   {
+          'assets/css/main.css':     ['.src/assets/less/main.less'],
+          'assets/css/page.css':     ['.src/assets/less/page.less'],
           'assets/css/examples.css': ['.src/assets/less/examples.less'],
-          'assets/css/treemap.css': ['.src/assets/examples/driven-by-data/styles/main.scss']
+          'assets/css/treemap.css':  ['.src/assets/examples/driven-by-data/styles/main.scss']
         }
       }
     },
-    uglify: {
+    uglify:   {
       dist: {
         options: {
-          mangle: false,
+          mangle:   false,
           compress: false
         },
-        files: {
-          'assets/js/scripts.min.js': [
+        files:   {
+          'assets/js/scripts.min.js':                           [
             '.src/assets/js/vendor/modernizr-2.8.3.js',
             '.src/assets/js/vendor/jquery-1.11.3.min.js',
             '.src/assets/js/plugins/*.js',
             '.src/assets/js/_*.js'
           ],
-          'assets/examples/driven-by-data/scripts/main.min.js':
-              [
-                '.src/assets/examples/driven-by-data/scripts/d3.min.js',
-                '.src/assets/examples/driven-by-data/data/yahooResponseStatic.js',
-                '.src/assets/examples/driven-by-data/data/SUB_INDUSTRY_MAP.js',
-                '.src/assets/examples/driven-by-data/data/FLARE_SECTOR_MAP.js',
-                '.src/assets/examples/driven-by-data/data/symbol2subindustry.js',
-                '.src/assets/examples/driven-by-data/data/holdings.js',
-                '.src/assets/examples/driven-by-data/scripts/main.js'
-              ]
+          'assets/examples/driven-by-data/scripts/main.min.js': [
+            '.src/assets/examples/driven-by-data/scripts/d3.min.js',
+            '.src/assets/examples/driven-by-data/data/yahooResponseStatic.js',
+            '.src/assets/examples/driven-by-data/data/SUB_INDUSTRY_MAP.js',
+            '.src/assets/examples/driven-by-data/data/FLARE_SECTOR_MAP.js',
+            '.src/assets/examples/driven-by-data/data/symbol2subindustry.js',
+            '.src/assets/examples/driven-by-data/data/holdings.js',
+            '.src/assets/examples/driven-by-data/scripts/main.js'
+          ]
         }
       }
     },
@@ -64,18 +64,18 @@ module.exports = function(grunt) {
       dist: {
         options: {
           optimizationLevel: 7,
-          quiet: true,
-          progressive: true
+          quiet:             true,
+          progressive:       true
         },
-        files: [{
+        files:   [{
           expand: true,
-          cwd: '.src/images/',
-          src: '{,*/}*.{png,jpg,jpeg}',
-          dest: 'images/'
+          cwd:    '.src/images/',
+          src:    '{,*/}*.{png,jpg,jpeg}',
+          dest:   'images/'
         }]
       }
     },
-    uncss: {
+    uncss:    {
       dist: {
         src:  [
           '.src/_includes/browser-upgrade.html',
@@ -92,92 +92,92 @@ module.exports = function(grunt) {
         dest: 'assets/css/main.UN.css'
       }
     },
-    inline: {
-      example: {
-        expand: true,
+    inline:   {
+      example:  {
+        expand:  true,
         options: {
-          tag: '__inline=true',
+          tag:     '__inline=true',
           verbose: false,
-          defer: true,
-          async: true,
-          uglify: true,
-          cssmin: true,
-          exts: ['md']
+          defer:   true,
+          async:   true,
+          uglify:  inProduction,
+          cssmin:  inProduction,
+          exts:    ['md']
         },
-        cwd: '.src',
-        src: ['assets/examples/**/*.md'],
-        dest: './'
+        cwd:     '.src',
+        src:     ['assets/examples/**/*.md'],
+        dest:    './'
       },
       example2: {
-        expand: true,
+        expand:  true,
         options: {
-          tag: '__inline=true',
+          tag:     '__inline=true',
           verbose: false,
-          defer: true,
-          async: true,
-          uglify: true,
-          cssmin: true,
-          exts: ['md2']
+          defer:   true,
+          async:   true,
+          uglify:  inProduction,
+          cssmin:  inProduction,
+          exts:    ['md2']
         },
-        cwd: '.src',
-        src: ['.examples.md2'],
-        dest: './'
+        cwd:     '.src',
+        src:     ['.examples.md2'],
+        dest:    './'
       },
-      dist: {
-        expand: true,
+      dist:     {
+        expand:  true,
         options: {
-          tag: '',
+          tag:     '',
           verbose: false,
-          defer: true,
-          async: true,
-          uglify: true,
-          cssmin: true,
-          exts: ['html', 'md']
+          defer:   true,
+          async:   true,
+          uglify:  inProduction,
+          cssmin:  inProduction,
+          exts:    ['html', 'md']
         },
-        cwd: '.src',
-        src: ['*.md', '_includes/*.html', 'assets/examples/**/*.md'],
-        dest: './'
+        cwd:     '.src',
+        src:     ['*.md', '_includes/*.html', 'assets/examples/**/*.md'],
+        dest:    './'
       }
     },
 
-    shell: {
-      copyLayouts: {
+    shell:   {
+      copyLayouts:    {
         command: 'cp -Rv .src/_layouts .;'
       },
-      moveExamples: {
+      moveExamples:   {
         command: 'mv -v ./.examples.md2 ./examples.md'
       },
-      jekyllServe: {
-        command: 'jekyll serve --safe --trace --no-watch --config _config-dev.yml'
+      jekyllServe:    {
+        command: 'jekyll serve --safe --trace -w --config _config-dev.yml'
       },
-      jekyllStop: {
+      jekyllStop:     {
         command: "killall jekyll || exit 0;"
       },
       fontsProtected: {
-        command: "cat assets/css/main.UN.css assets/css/page.css assets/css/examples.css assets/css/treemap.css .src/assets/fonts/fontawesome-4.3-subset-b64.css .src/assets/fonts/font-awesome-subset.css  > assets/css/main.css"
+        command: "cat .src/assets/js/plugins/responsive-nav.css assets/css/main.UN.css assets/css/page.css assets/css/examples.css assets/css/treemap.css .src/assets/fonts/fontawesome-4.3-subset-b64.css .src/assets/fonts/font-awesome-subset.css  > assets/css/main.css"
       },
-      copyExamples: {
-        command: 'cp -vR .src/assets/examples/** assets/examples/'
+      copyExamples:   {
+        command: 'mkdir assets assets/examples assets/examples/css assets/js ;cp -vR .src/assets/examples/** assets/examples/'
       },
       setPermissions: {
         command: 'chmod -vR +x assets images'
       }
     },
-    copyto: {
-      exampleImages: { 
-        files: [
+    copyto:  {
+      exampleImages: {
+        files:   [
           {
-            cwd: '.src/assets/examples/', 
-            src: ['**/*.png'], 
-            dest: 'assets/examples/',
+            cwd:    '.src/assets/examples/',
+            src:    ['**/*.png'],
+            dest:   'assets/examples/',
             expand: true
           }
         ],
         options: {
-          processContent: function(content, path) {
+          processContent: function (content, path) {
             return content; // do something with content or return false to abort copy
           },
-          ignore: [
+          ignore:         [
             '**/*.md'
           ]
         }
@@ -185,59 +185,64 @@ module.exports = function(grunt) {
     },
     htmlmin: {
       dist: {
-        cwd:  '.',
-        src: ['_includes/*.html', '_layouts/*.html'], //no dest: change-in-place
-        expand: true,
+        cwd:     '.',
+        src:     ['_includes/*.html', '_layouts/*.html'], //no dest: change-in-place
+        expand:  true,
         options: {
-          removeComments: true,
-          removeEmptyAttributes: false,
-          collapseWhitespace: true,
+          removeComments:          true,
+          removeEmptyAttributes:   false,
+          collapseWhitespace:      true,
           removeCommentsFromCDATA: true,
-          removeIgnored: false,
-          customAttrSurround: [jekyllConditionalWrapPair],
-          minifyCSS: true,
-          minifyJS: {
-            sequences: true,
-            dead_code: true,
+          removeIgnored:           false,
+          customAttrSurround:      [jekyllConditionalWrapPair],
+          minifyCSS:               false,
+          minifyJS:                (inProduction ? {
+            sequences:     true,
+            dead_code:     true,
             drop_debugger: true,
-            conditionals: true,
-            comparisons: true,
-            booleans: true,
-            loops:true,
-            unused: false,
-            if_return: true,
-            join_vars: true,
-            cascade: true,
-            warnings: true,
-            drop_console: true,
-            keep_fargs: true
-          }
+            conditionals:  true,
+            comparisons:   true,
+            booleans:      true,
+            loops:         true,
+            unused:        false,
+            if_return:     true,
+            join_vars:     true,
+            cascade:       true,
+            warnings:      true,
+            drop_console:  true,
+            keep_fargs:    true
+          } : false)
         }
       }
     },
-    
+
     watch: {
       configFiles: {
-        files: [ 'Gruntfile.js', '_config.yml', 'package.json' ],
+        files:   ['Gruntfile.js', '_config.yml', 'package.json'],
         options: {
           reload: true
         }
       },
-      html: {
-        files: ['**/*.html', '**/*.md']
+      html:        {
+        files: ['.src/**/*.html', '.src/**/*.md'],
+        tasks: ['inline', 'htmlmin']
       },
-      less: {
+      less:        {
         files: [
-          'assets/less/*.less',
-          'assets/examples/driven-by-data/styles/main.scss'
+          '.src/assets/less/*.less',
+          '.src/assets/examples/driven-by-data/styles/main.scss'
         ],
-        tasks: ['less']
+        tasks: ['less', 'shell:fontsProtected', 'inline', 'htmlmin']
       },
-      js: {
+      css: {
+        files: ['.src/assets/js/plugins/ononsive-nav.css'],
+        tasks: ['shell:fontsProtected']
+      },
+      js:          {
         files: [
           '<%= jshint.all %>'
         ],
-        tasks: ['uglify']
+        tasks: ['inline', 'htmlmin']
       }
     },
     clean: {
@@ -246,29 +251,30 @@ module.exports = function(grunt) {
         '_site/**',
         '_includes/*',
         '_layouts/*',
-        'assets/examples/*',
+        'assets/**',
         'images/*',
         'examples/**'
       ]
     }
-  });
+
+  } );
 
 // Load tasks
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-shell');
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-htmlmin');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-copy-to');
-  grunt.loadNpmTasks('grunt-uncss');
-  grunt.loadNpmTasks('grunt-inline');
-  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks( 'grunt-contrib-clean' );
+  grunt.loadNpmTasks( 'grunt-shell' );
+  grunt.loadNpmTasks( 'grunt-contrib-less' );
+  grunt.loadNpmTasks( 'grunt-contrib-htmlmin' );
+  grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+  grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+  grunt.loadNpmTasks( 'grunt-contrib-watch' );
+  grunt.loadNpmTasks( 'grunt-copy-to' );
+  grunt.loadNpmTasks( 'grunt-uncss' );
+  grunt.loadNpmTasks( 'grunt-inline' );
+  grunt.loadNpmTasks( 'grunt-contrib-imagemin' );
 
 // Register tasks
-  grunt.registerTask('buildify', [
-    'clean',  
+  grunt.registerTask( 'buildify', [
+    'clean',
     'shell:copyExamples',
     'less',                     // compile less to CSS
     'uglify',                   // really just a concat
@@ -282,24 +288,24 @@ module.exports = function(grunt) {
     'htmlmin:dist',             // compress all HTML/CSS/JS
     'copyto'                    // include images for below-the-fold loading
     //'shell:setPermissions'
-  ]);
+  ] );
 
-  grunt.registerTask('default', [
-      'shell:jekyllStop',
-      'buildify',
-      'shell:jekyllServe',      // serve in development
-      'watch'
-    ]);
-  
+  grunt.registerTask( 'default', [
+    'shell:jekyllStop',
+    'buildify',
+    'shell:jekyllServe',      // serve in development
+    'watch'
+  ] );
+
   /** PRODUCTION DEPLOYMENT **/
-  grunt.registerTask('PROD', [
+  grunt.registerTask( 'PROD', [
     'buildify'
-  ]);
+  ] );
 
   /** PRODUCTION ROLLBACK **/
-  grunt.registerTask('ROLLBACK', [
+  grunt.registerTask( 'ROLLBACK', [
     'shell: git checkout master~1',   //TODO: change this to branch~1 after first successful gh-pages push
     'ROLLBACK'
-  ]);
+  ] );
 
 };
